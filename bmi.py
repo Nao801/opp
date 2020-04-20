f081e98161e7f117b9ece386ca3402f8ad71afe6
Nao801 <nicksm801@gmail.com>
2020-04-20 20:08:36 +0900

    bmi計算が完了２

--- bmi.py
+++ bmi.py
@@ -20,14 +20,14 @@ class BMI:
     def __init__(self, height, weight):
         self.height = height
         self.weight = weight
-
-    def calculate_bmi(self):
-        return self.weight / (self.height ** 2)
+        self.value = self.weight / (self.height ** 2)
 
 
 # BMIクラスのインスタンス化
 hibiki_bmi = BMI(height=1.80, weight=67.0)
-ohira_bmi = BMI(height=1.78, weight=75.0)
-
 print(hibiki_bmi.height, hibiki_bmi.weight)
-print(hibiki_bmi.calculate_bmi())
+print(hibiki_bmi.value)
+
+ohira_bmi = BMI(height=1.78, weight=75.0)
+print(ohira_bmi.height, ohira_bmi.weight)
+print(ohira_bmi.value)
