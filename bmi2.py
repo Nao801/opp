764694c5e7302c7fc093bd1903b000312f45d320
Nao801 <nicksm801@gmail.com>
2020-04-20 20:28:40 +0900

    bmi計算が完了4

--- bmi2.py
+++ bmi2.py
@@ -20,6 +20,9 @@ class BMI:
     def __init__(self, height, weight):
         self.value = weight / (height ** 2)
 
+        if not (10 <= self.value <= 40):
+            raise ValueError('BMIが正常値の範囲を超えています')
+
     def __str__(self):
         return f'{self.value:.2f}'
 
