ed59758122d869f8bf805b091b12b3652ba7f0ba
Nao801 <nicksm801@gmail.com>
2020-04-20 21:14:43 +0900

    rectangle途中

--- kadai.py
+++ kadai.py
@@ -47,3 +47,23 @@ rectangle2.area() #9.00
 rectangle2.diagonal() #4.24
 
 '''
+class Rectangle:
+    def __init__(self, height, width):
+        self.height= height
+        self.width = width
+
+    def area(self):
+        return self.height*self.width
+
+    def diagonal(self):
+        return  (self.height**2 + self.width**2)
+
+
+assert circle1.area()==30.00
+
+rectangle1 = Rectangle(height=5, width=6)
+print(rectangle1.area())
+print(rectangle1.diagonal())
+rectangle2 = Rectangle(height=3, width=3)
+print(rectangle2.area())
+print(rectangle2.diagonal())
